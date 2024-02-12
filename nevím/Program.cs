d9a5c37603df348b0b07ac54641ab15112cbb967
Balajka Martin <2023BalajkaMartin@skolabaltaci.local>
2024-02-12 10:57:17 +0100

    Přidejte soubory projektu.

---
+++ nevím/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nevím
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* byte a = 20;
+             long b = 65536;
+             int g = 35;
+
+
+             float c = 5.45f;
+             double d = 5.334;
+
+             bool test = true;
+
+             char f = 'F';
+
+             string text = "Zdar a sílu";
+
+             long x = b;
+
+             int vysledek = a + g;
+
+             Console.WriteLine("Prom a je"+a);
+             Console.WriteLine("Prom b je"+b);
+             Console.WriteLine("Prom c je"+c);
+             Console.WriteLine("Prom d je"+d);
+             Console.WriteLine("Prom test je"+test);
+             Console.WriteLine("Prom f je"+f);
+             Console.WriteLine("Prom text je"+text);
+             Console.WriteLine("Prom x je"+x);
+             Console.WriteLine("Prom vysledek je" + vysledek);
+            */
+            /*
+            Console.WriteLine("zadej jak před jak dlouho bylo mezi bleskem a hromem v sekundách");
+            double cisloJenda = byte.Parse(Console.ReadLine());
+        
+            double vysledek = cisloJenda *(float) 0.350;
+
+            Console.WriteLine("bouřka je přibližně metrů :" + vysledek);
+            Console.ReadKey();
+            */
+            
+            byte a = 20;
+            if (a > 5)
+            {
+                Console.WriteLine("A je Větší než 5");
+            }
+            if (a == 3)
+            {
+                Console.WriteLine("A je rovno 3");
+            }
+            if (a < 5)
+            {
+                Console.WriteLine("A je menší než  5");
+            }
+
+            Console.WriteLine("Za podmínkou");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
